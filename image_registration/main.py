6f529955fc36e97b173c5b7484914182f5e27d7f
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-06-07 22:04:26 +0530

    added module

---
+++ image_registration/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from image_registration import *
+import glob
+
+direc="tests"
+reg_images=glob.glob(direc+"/*_reg.fits")
+
+ref='g'
+
+#register_reproject(direc)
+
+for image in reg_images:
+	reg_check_diff(image, direc+"/"+ref+".fits_reg.fits", image+"_diff")
