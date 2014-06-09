a4a1bbe94a0f02dffe775dd986d6f0461c6baae7
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-06-10 02:07:08 +0530

    setup modified

--- setup-deps.sh
+++ setup-deps.sh
@@ -1,8 +1,5 @@
 # Setup script for image-photo-z. Assuming a Debian-like operating system.
 
-# Get root permissions
-sudo sudo > /dev/null
-
 # Get latest definitions
 sudo apt-get update
 sudo apt-get upgrade python
