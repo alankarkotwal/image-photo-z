a2c6744374cb933e36aa376c6a9723c317ca0c23
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-06-25 09:44:20 +0530

    sync all

--- catalog_gen/sql/QueryDR10.sql
+++ catalog_gen/sql/QueryDR10.sql
@@ -1,13 +1,19 @@
-SELECT ALL
+SELECT TOP 10
   spec.specObjID AS id,
   spec.ra,
   spec.dec,
   spec.z AS redshift,
-  spec.zErr as redshiftError,
+  spec.zErr AS redshiftError,
   specAll.class,
   specAll.run,
   specAll.camcol,
-  specAll.field
+  specAll.field,
+  specAll.dered_u AS u,
+  specAll.dered_g AS g,
+  specAll.dered_r AS r,
+  specAll.dered_i AS i,
+  specAll.dered_z AS z
+INTO objects_with_redshifts
 FROM SpecObj AS spec
 JOIN SpecPhotoAll AS specAll
 ON spec.specObjID = specAll.specObjID,
